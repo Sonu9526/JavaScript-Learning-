4866252e9015a3594ae7adb9b3c7c51cf096b522
Sonu S Nair <155825583+Sonu9526@users.noreply.github.com>
2025-07-06 18:48:27 +0530

    Add files via upload

---
+++ Data Structure and string/StringPart1.js
@@ -0,0 +1,12 @@
+const airline = "Air indiA";
+const plane = "A320";
+console.log(airline.length);
+console.log("Sonu".length);
+
+//String methods
+console.log(airline.indexOf("i"));
+console.log(airline.lastIndexOf("i"));
+const nam = "Sonu";
+console.log(nam.slice(0, 3)); // return Son
+console.log(nam.slice(0, nam.indexOf("n") + 1)); //same as above
+console.log(nam.slice(-1));
