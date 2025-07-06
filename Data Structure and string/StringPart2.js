41ecb4da67c252b3f94309355f0ad34a8bc77260
Sonu S Nair <155825583+Sonu9526@users.noreply.github.com>
2025-07-06 19:14:53 +0530

    Add files via upload

---
+++ Data Structure and string/StringPart2.js
@@ -0,0 +1,8 @@
+//Compare emails
+const email = "sonusnair46@gmail.com";
+const loginEmail = "   Sonusnair46@gmail.com  \n";
+const normalEmail = loginEmail.toLowerCase().trim();
+console.log(normalEmail);
+
+const names = "Sonu";
+console.log(names.replace("Sonu", "kannan"));
